c71b8a191238a785fdbe4766d0f30692767ebbed
Juan Facundo Suárez <facundo@svarez.com.ar>
2013-08-16 09:43:07 -0300

    todo

--- lovecino_theme/index.php
+++ lovecino_theme/index.php
@@ -15,7 +15,7 @@
 <html class="ie ie8" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="csstransforms">
 <!--<![endif]-->
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -24,10 +24,26 @@
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <script src="<?php echo get_template_directory_uri(); ?>/js/main.js" type="text/javascript"></script>
   </head>
 
-  <body class="initial-page">
+  <body class="initial-page component">
     <div id="main" class="site-main">
     </div>
+
+    <button class="cn-button" id="cn-button">Menu</button>
+    <div class="cn-wrapper" id="cn-wrapper">
+      <ul>
+        <li><a href="#"><span>About</span></a></li>
+        <li><a href="#"><span>Tutorials</span></a></li>
+        <li><a href="#"><span>Articles</span></a></li>
+        <li><a href="#"><span>Snippets</span></a></li>
+        <li><a href="#"><span>Plugins</span></a></li>
+        <li><a href="#"><span>Contact</span></a></li>
+        <li><a href="#"><span>Follow</span></a></li>
+       </ul>
+    </div>
+
+  <script>require('main');</script>
   </body>
 </html>
--- sources/javascript/index.js
+++ sources/javascript/index.js
@@ -3,6 +3,7 @@
  * Module dependencies
  */
 
+var o = require('jquery');
 var routing = require('./routing');
 
 /**
@@ -10,3 +11,45 @@ var routing = require('./routing');
  */
 
 routing();
+
+initCircularMenu();
+
+function initCircularMenu(){
+  var button = o('.cn-button');
+  var wrapper = o('.cn-wrapper');
+  var overlay = o('.cn-overlay');
+
+  var open = false;
+  button.on('click', handler);
+  wrapper.on('click',function(e){
+    e.stopPropagation();
+  });
+
+  function handler(e){
+    if (!e) var e = window.event;
+    e.stopPropagation();//so that it doesn't trigger click event on document
+
+    if(!open) {
+      openNav();
+    }
+    else {
+      closeNav();
+    }
+  }
+
+  function openNav(){
+    open = true;
+    button.html('-');
+    overlay.addClass('on-overlay');
+    wrapper.addClass('opened-nav');
+  }
+
+  function closeNav(){
+    open = false;
+    button.html('+');
+    overlay.removeClass('on-overlay');
+    wrapper.removeClass('opened-nav');
+  }
+
+  document.addEventListener('click', closeNav);
+}
